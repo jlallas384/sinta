a6778d75b99aecd00034b1be4a3a9b0286b2a454
jlallas384 <jlallas384@gmail.com>
2025-06-26 04:31:56 +0800

    padding login

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,36 +1,34 @@
 import { useState } from "react";
-import logoText from '../assets/logo-text.svg'
+import logoText from '../assets/logo-text.svg';
 import { useNavigate } from "react-router";
 
 function Index() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
 
   const validateEmail = () => {
     const isValidFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    const okay = email.endsWith('iskolarngbayan.pup.edu.ph')
+    const okay = email.endsWith('iskolarngbayan.pup.edu.ph');
 
     if (!okay || !isValidFormat) {
-      setError("Invalid Webmail")
+      setError("Invalid Webmail");
     } else {
-      navigate('/features')
+      navigate('/features');
     }
   };
 
   return (
-    <div>
-      <img src={logoText} className="mx-auto mt-5 mb-30"/>
-      <div className="mx-auto text-center w-90 gap-5 flex flex-col">
-        <h1 className="font-bold text-2xl text-yellow-300">
-          LOGIN
-        </h1>
+    <div className="flex justify-center px-4">
+      <div className="w-full max-w-md text-center space-y-5">
+        <img src={logoText} className="mx-auto mt-5 mb-10" />
+        <h1 className="font-bold text-2xl text-yellow-300 mt-30">LOGIN</h1>
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="bg-white w-full border-black border rounded-lg p-3"
+          className="bg-white w-full border border-black rounded-lg p-3"
           placeholder="PUP Webmail"
         />
         {error && (
@@ -39,7 +37,7 @@ function Index() {
         <button
           type="button"
           onClick={validateEmail}
-          className="w-full bg-red-500 rounded-lg p-3 text-white cursor-pointer border-black border"
+          className="w-full bg-red-500 rounded-lg p-3 text-white cursor-pointer border border-black"
         >
           LOG IN
         </button>
